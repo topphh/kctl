43744073aa523ec3246e7717908bc6707c3135b0
topphh <email.cristopher@gmail.com>
2025-06-19 18:59:08 +0800

    refactor table display

--- utils/display/display.go
+++ utils/display/display.go
@@ -7,16 +7,8 @@ import (
 )
 
 func PrintTable(data [][]string) {
-
 	table := tablewriter.NewWriter(os.Stdout)
-
-	for i, v := range data {
-		if i == 0 {
-			table.Header(v)
-			continue
-		}
-		table.Append(v)
-	}
-
+	table.Header(data[0])
+	table.Bulk(data[1:])
 	table.Render()
 }
